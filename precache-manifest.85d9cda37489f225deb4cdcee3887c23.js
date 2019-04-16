8981c5140ac10e9218df8b2c78dd420abeea61a3
stevenkulp <sdkulp@gmail.com>
2019-04-16 15:50:06 -0700

    Updates

---
+++ precache-manifest.85d9cda37489f225deb4cdcee3887c23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f27ae0212227de605566",
+    "url": "/TicTacToe/static/css/main.189351ed.chunk.css"
+  },
+  {
+    "revision": "f27ae0212227de605566",
+    "url": "/TicTacToe/static/js/main.f27ae021.chunk.js"
+  },
+  {
+    "revision": "abd9e857442f04b96f9b",
+    "url": "/TicTacToe/static/js/1.abd9e857.chunk.js"
+  },
+  {
+    "revision": "1a4430c086131fba29bb",
+    "url": "/TicTacToe/static/js/runtime~main.1a4430c0.js"
+  },
+  {
+    "revision": "c167530074534da7fd43f76b08c59996",
+    "url": "/TicTacToe/static/media/background.c1675300.jpg"
+  },
+  {
+    "revision": "7bc8098c472a1bc6cde9890b78f62047",
+    "url": "/TicTacToe/index.html"
+  }
+];
